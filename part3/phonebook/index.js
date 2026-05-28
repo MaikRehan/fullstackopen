40cc39d5d3911ecaac6a252b1493a9bd26c4d05b
Maik Rehan <mrehan@tagitron.de>
2026-05-28 15:36:16 +0200

    Exercises 3.9 + 3.10

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const morgan = require("morgan");
+const cors = require('cors');
 const app = express()
+
 
 morgan.token('body', (request, response) => {
     return JSON.stringify(request.body)
 })
 
+app.use(cors());
 app.use(express.json())
+app.use(express.static('dist'))
 
 app.use(morgan((tokens, request, response) => {
     if (request.method === 'POST') {
@@ -110,6 +114,6 @@ const generateId = () => {
     return String(id)
 }
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server is running on port ${PORT}`)
