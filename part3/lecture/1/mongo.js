6146f49ce40de09f3c82547eca3a50ac8ba9c7fb
MaikRehan <m.rehan@gmx.de>
2026-06-16 07:43:26 +0200

    Lecture 3c all

--- part3/lecture/1/mongo.js
+++ part3/lecture/1/mongo.js
@@ -7,11 +7,10 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
+const url = `mongodb+srv://fullstack:${password}@cluster0.a5qfl.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
-const url = `mongodb://maikrehan:${password}@ac-crafurz-shard-00-00.4oe0m33.mongodb.net:27017,ac-crafurz-shard-00-01.4oe0m33.mongodb.net:27017,ac-crafurz-shard-00-02.4oe0m33.mongodb.net:27017/noteApp?ssl=true&replicaSet=atlas-qabzc7-shard-0&authSource=admin&appName=Cluster0`
-mongoose.set('strictQuery',false)
-
-mongoose.connect(url, { family: 4 })
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
     content: String,
@@ -20,19 +19,18 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-//
-//const note = new Note({
-//    content: 'HTML is easy',
-//    important: true,
-//})
+// const note = new Note({
+//   content: 'HTML is easy',
+//   important: true,
+// })
 
-//note.save().then(result => {
-//    console.log('note saved!')
-//    mongoose.connection.close()
-//})
+// // note.save().then((result) => {
+// //   console.log('note saved!')
+// //   mongoose.connection.close()
+// // })
 
-Note.find({ important: false }).then(result => {
-    result.forEach(note => {
+Note.find({}).then((result) => {
+    result.forEach((note) => {
         console.log(note)
     })
     mongoose.connection.close()
