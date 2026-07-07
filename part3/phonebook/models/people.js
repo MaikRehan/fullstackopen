3aff815624ed2273079bd404408b3f092b25818c
MaikRehan <m.rehan@gmx.de>
2026-07-07 14:34:02 +0200

    Exercise 3.20 + 3.21

--- part3/phonebook/models/people.js
+++ part3/phonebook/models/people.js
@@ -20,7 +20,17 @@ const personSchema = new mongoose.Schema({
         minlength: 3,
         required: true,
     },
-    number: String,
+    number: {
+        type: String,
+        minlength: 8,
+        required: true,
+        validate: {
+            validator: function (value) {
+                return /^\d{2,3}-\d+$/.test(value)
+            },
+            message: props => `${props.value} ist keine gültige Telefonnummer!`
+        }
+    },
 })
 
 personSchema.set('toJSON', {
