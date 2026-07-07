3e43c048bffb985c1d52c28de5294067d4a61a4c
MaikRehan <m.rehan@gmx.de>
2026-07-07 13:28:48 +0200

    Lecture 3d Start

--- part3/lecture/1/index.js
+++ part3/lecture/1/index.js
@@ -41,7 +41,7 @@ app.get('/api/notes/:id', (request, response) => {
         .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
 
     if (!body.content) {
@@ -53,9 +53,11 @@ app.post('/api/notes', (request, response) => {
         important: body.important || false,
     })
 
-    note.save().then((savedNote) => {
+    note.save()
+        .then((savedNote) => {
         response.json(savedNote)
     })
+        .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -96,6 +98,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
