396694b7de235df5d8cb4e325806c08114fab6a8
MaikRehan <m.rehan@gmx.de>
2026-07-16 12:25:15 +0200

    Exercise 4.5

--- part4/blogpost/utils/list_helper.js
+++ part4/blogpost/utils/list_helper.js
@@ -8,7 +8,15 @@ const totalLikes = (blogs) => {
     }, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length < 1) {return null}
+    return blogs.reduce((favorite, blog) => {
+        return blog.likes > favorite.likes ? blog : favorite
+    })
+}
+
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
 }
