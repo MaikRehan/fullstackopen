ef9a961bbdc73496a3cfa882d64234e48e4bad05
MaikRehan <m.rehan@gmx.de>
2026-07-17 11:06:04 +0200

    Lecture 4b: Start - supertest

--- part3/lecture/1/mongo.js
+++ part3/lecture/1/mongo.js
@@ -1,16 +1,7 @@
 const mongoose = require('mongoose')
-
-if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
-}
-
-const password = process.argv[2]
-
-const url = `mongodb+srv://fullstack:${password}@cluster0.a5qfl.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+const config = require('./utils/config')
 
 mongoose.set('strictQuery', false)
-mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
     content: {
@@ -23,19 +14,19 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-// const note = new Note({
-//   content: 'HTML is easy',
-//   important: true,
-// })
+const main = async () => {
+    await mongoose.connect(config.MONGODB_URI)
 
-// // note.save().then((result) => {
-// //   console.log('note saved!')
-// //   mongoose.connection.close()
-// // })
+    await new Note({ content: 'HTML is easy', important: true }).save()
+    await new Note({ content: 'Browser can execute only JavaScript', important: true }).save()
+    console.log('notes saved!')
 
-Note.find({}).then((result) => {
-    result.forEach((note) => {
+    const notes = await Note.find({})
+    notes.forEach((note) => {
         console.log(note)
     })
-    mongoose.connection.close()
-})
+
+    await mongoose.connection.close()
+}
+
+main()
