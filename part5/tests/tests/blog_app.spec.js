0aee6fe6336e7a27577c7bf9355e378f247724a8
MaikRehan <m.rehan@gmx.de>
2026-09-07 13:47:14 +0200

    Exercise 5.17

---
+++ part5/tests/tests/blog_app.spec.js
@@ -0,0 +1,14 @@
+const {test, expect, beforeEach, describe} = require('@playwright/test')
+
+describe('Blog app', () => {
+    beforeEach(async ({page}) => {
+        await page.goto('http://localhost:5173')
+    })
+
+    test('Login form is shown', async ({page}) => {
+        await page.goto('http://localhost:5173')
+
+        const locator = page.getByText('Log in to application')
+        await expect(locator).toBeVisible()
+    })
+})
