bc438535746213c7d574391f14127165bbbe512a
MaikRehan <m.rehan@gmx.de>
2026-09-11 06:41:16 +0200

    Lecture 5e - UI libraries

--- part2/lecture/src/App.jsx
+++ part2/lecture/src/App.jsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from 'react'
+import {useState, useEffect} from 'react'
 import noteService from './services/notes'
+import {AppBar, Button, Container, Toolbar} from '@mui/material'
 
 import {
     Routes, Route, Link, useMatch
@@ -10,9 +11,11 @@ import Home from './components/Home'
 import Footer from './components/Footer'
 import NoteForm from './components/NoteForm'
 import Note from './components/Note'
+import Notification from './components/Notification'
 
 const App = () => {
     const [notes, setNotes] = useState([])
+    const [notification, setNotification] = useState(null)
 
     useEffect(() => {
         const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
@@ -31,6 +34,10 @@ const App = () => {
     const addNote = noteObject => {
         noteService.create(noteObject).then(returnedNote => {
             setNotes(notes.concat(returnedNote))
+            setNotification({ text: `Note '${returnedNote.content}' added!`, type: 'success' })
+            setTimeout(() => {
+                setNotification(null)
+            }, 5000)
         })
     }
 
@@ -42,7 +49,7 @@ const App = () => {
 
     const toggleImportanceOf = id => {
         const note = notes.find(n => n.id === id)
-        const changedNote = { ...note, important: !note.important }
+        const changedNote = {...note, important: !note.important}
 
         noteService
             .update(id, changedNote)
@@ -74,34 +81,47 @@ const App = () => {
         : null
 
     console.log(note)
+    const style = { '&:hover': { bgcolor: 'rgba(255,255,255,0.3)' } }
 
     return (
-        <div>
+        <Container>
             <div>
-                <Link style={padding} to="/">home</Link>
-                <Link style={padding} to="/notes">notes</Link>
-                <Link style={padding} to="/create">new note</Link>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Button color="inherit" component={Link} to="/" sx={style}>
+                            home
+                        </Button>
+                        <Button color="inherit" component={Link} to="/notes" sx={style}>
+                            notes
+                        </Button>
+                        <Button color="inherit" component={Link} to="/create" sx={style}>
+                            new note
+                        </Button>
+                    </Toolbar>
+                </AppBar>
+
+                <Notification notification={notification} />
+
+                <Routes>
+                    <Route path="/notes/:id" element={
+                                                  <Note
+                                                     note={note}
+                                                     toggleImportanceOf={toggleImportanceOf}
+                                                     deleteNote={deleteNote}
+                                                 />
+                                             }/>
+                    <Route path="/notes" element={
+                                              <NoteList notes={notes}/>
+                                         }/>
+                    <Route path="/create" element={
+                                               <NoteForm createNote={addNote}/>
+                                          }/>
+                    <Route path="/" element={<Home/>}/>
+                </Routes>
+
+                <Footer/>
             </div>
-
-            <Routes>
-                <Route path="/notes/:id" element={
-                                              <Note
-                                                 note={note}
-                                                 toggleImportanceOf={toggleImportanceOf}
-                                                 deleteNote={deleteNote}
-                                             />
-                                         } />
-                <Route path="/notes" element={
-                                          <NoteList notes={notes} />
-                                     } />
-                <Route path="/create" element={
-                                           <NoteForm createNote={addNote}/>
-                                      } />
-                <Route path="/" element={<Home />} />
-            </Routes>
-
-            <Footer />
-        </div>
+        </Container>
     )
 }
 
--- part2/lecture/src/components/NoteList.jsx
+++ part2/lecture/src/components/NoteList.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
 
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material'
+
 import { Link } from 'react-router-dom'
 
 import Note from './Note'
@@ -65,22 +67,34 @@ const NoteList = ({ notes }) => {
     return (
         <div>
             <h1>Notes</h1>
-            <Notification message={errorMessage} />
-
-            {!user && loginForm()}
-
-            <div>
-                <button onClick={() => setShowAll(!showAll)}>
-                    show {showAll ? 'important' : 'all'}
-                </button>
-            </div>
-            <ul>
-                {notesToShow.map(note => (
-                    <li key={note.id}>
-                        <Link to={`/notes/${note.id}`}>{note.content}</Link>
-                    </li>
-                ))}
-            </ul>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>content</TableCell>
+                            <TableCell>user</TableCell>
+                            <TableCell>important</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {notes.map(note => (
+                            <TableRow key={note.id}>
+                                <TableCell>
+                                    <Link to={`/notes/${note.id}`}>
+                                        {note.content}
+                                    </Link>
+                                </TableCell>
+                                <TableCell>
+                                    {note.user.name}
+                                </TableCell>
+                                <TableCell>
+                                    {note.important ? 'yes': ''}
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </div>
     )
 }
