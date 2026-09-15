adc5b3b6d0d8b2aaf3f191a4e8b4124453bca67b
MaikRehan <m.rehan@gmx.de>
2026-09-15 08:01:20 +0200

    Lecture 9.3: Start - Exercise 1

---
+++ part9/lecture/calculator.ts
@@ -0,0 +1,29 @@
+type Operation = 'multiply' | 'add' | 'divide';
+
+const calculator = (a: number, b: number, operation: Operation): number => {
+    switch (operation) {
+        case "multiply":
+            return a * b;
+        case "divide":
+            if (b === 0) throw new Error('can\'t divide by 0!');
+            return a / b;
+        case "add":
+            return a + b;
+        default:
+            throw new Error('unknown operation: ' + operation);
+    }
+}
+try {
+    console.log(calculator(1, 5, 'divide'));
+} catch (error: unknown) {
+    let errorMessage = 'Something went wrong';
+    if (error instanceof Error) {
+        errorMessage += error.message;
+    }
+}
+
+const a: number = Number(process.argv[2])
+const b: number = Number(process.argv[3])
+
+calculator(a, b, 'divide');
+
