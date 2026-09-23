1a61eb355363f67010fec5430859ceb65f03bbc2
MaikRehan <m.rehan@gmx.de>
2026-09-23 14:38:06 +0200

    Exercise 9.3.8: Checkup

--- part9/healthapp/index.ts
+++ part9/healthapp/index.ts
@@ -6,48 +6,65 @@ const app = express();
 app.use(express.json());
 
 app.get('/hello', (_req, res) => {
-    res.send('Hello Full Stack');
+    res.send('Hello Full Stack!');
 });
 
 app.get('/bmi', (req, res) => {
-    const height = Number(req.query.height);
-    const weight = Number(req.query.weight);
-    const response = bmiCalculator.calculateBmi(weight, height);
+    const height = req.query.height;
+    const weight = req.query.weight;
 
-    if (isNaN(height) || isNaN(weight)) {
-        res.send({
-            error: 'malformatted parameters',
-        }).status(400);
+    if (!height || !weight) {
+        res.status(400).send({
+            error: 'malformatted parameters'
+        });
+    } else if ((isNaN(Number(height))) || isNaN(Number(weight))) {
+        res.status(400).send({
+            error: 'malformatted parameters'
+        });
     } else {
-        res.send(response).status(200);
+        const response = bmiCalculator.calculateBmi(Number((height)), Number(weight));
+        res.send({
+                bmi: response.bmi,
+                weight: Number(weight),
+                height: Number(height)
+            }
+        ).status(200);
     }
 });
 
 app.post('/exercises', (req, res) => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const { dailyExercises, target } = req.body;
+    const {daily_exercises, target} = req.body;
 
-    if ( !target || !dailyExercises)  {
-        console.log(target);
-        console.log(dailyExercises);
+    if (!target || !daily_exercises) {
         return res.status(400).send(
             {
-                error: 'missing values',
+                error: 'parameters missing',
             }
         );
     }
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
-    if (dailyExercises.some(isNaN))  {
-        return res.status(400).send({ error: 'malformed parameters',});
-    }
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    const result = calcExercises(target, dailyExercises);
+    if (daily_exercises.some((value) => isNaN(Number(value))) || isNaN(Number(target))) {
 
-    return res.send({ result });
+        return res.status(400).send({error: 'malformatted parameters',});
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+    const result = calcExercises(Number(target),  daily_exercises.map(Number));
+
+    return res.send(
+        {
+            periodLength: Number(result.periodLength),
+            trainingDays: Number(result.trainingDays),
+            success: Boolean(result.success),
+            rating: Number(result.rating),
+            ratingDescription: String(result.comment),
+            target: Number(result.target),
+            average: Number(result.average),
+        });
 
 });
 
-const PORT = 3003;
+const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
